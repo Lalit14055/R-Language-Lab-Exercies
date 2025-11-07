43cc2bfda72b4619c8bcc2d420fcd110a9e5966d
Lalit14055 <patillalit1405@gmail.com>
2025-11-07 18:19:37 +0530

    added new R Exercise

--- lab 7.R
+++ lab 7.R
@@ -1,11 +1,28 @@
 #7:Statistical Tests in R
 
+library(dplyr)
+#Load necessary dataset
+data("iris")
 
+#1.t-test: Compare Sepal.Length of setosa and versicolor
 
+t_test_result <-t.test(Sepal.length~Species,
+                       data=iris%>%filter(species%in% c("setosa",
+                                                        "versicolor")))
+t_test_result
 
+#2.ANOVA:Compare Sepal.Length across all species
 
+anova_model <- aov(Sepal.Length Species, data = iris)
+summary(anova_model)
 
+#3.Correlation:Sepal.Length and Petal.Length
+correlation <- cor(iris$Sepal.Length, iris$Petal.Length)
+correlation
 
+#4.Correlation test with significance
+cor_test <- cor.test(iris$Sepal.Length, iris Petal.Length)
+cor_test
 
 
 
---
+++ lab 8.R
@@ -0,0 +1,15 @@
+#8:Regression Analysis in R
+
+library(dplyr)
+library(ggplot2)
+
+data("mtcars")
+View(mtcars)
+
+
+
+
+
+
+
+
